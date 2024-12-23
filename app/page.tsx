2f87a84b80db3727396721cc9578e7ec2a655298
PuneetP16 <patelpunit95@gmail.com>
2024-12-23 10:08:17 +0530

    update: page name

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from 'react'
 import { Content } from '@/components/layouts/content'
 import { Header } from '@/components/layouts/header'
 
-export default async function TaskPage() {
+export default async function ContractsPage() {
   return (
     <div className='p-4 md:p-8'>
       <Suspense fallback={<div className='h-screen flex items-center justify-center'>Loading...</div>}>
